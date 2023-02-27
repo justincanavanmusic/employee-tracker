5c68a5b53ef5d79cda554abb1d2c54ff3c2a7499
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-27 09:43:22 -0600

    figured out syntax for validation prompt

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
 const inquirer = require('inquirer');
 const fs=require('fs');
+// const cTable = require('console.table');
+// const mysql = require('mysql2');
 
-const question = [    
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'test'
+//   });
+
+const questions = [    
     {
      type: 'list',
      message: 'What would you like to do?',
      choices: ['View All Employees', "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
-     name: 'choice'
+     name: 'option'
+      },
+    {
+     type: 'input',
+     message: 'What is the name of the department?',
+     name: 'dept',
+     validate: function (userAnswer) {
+      
+        if (userAnswer.length>0) {
+          console.log (`
+          Added "${userAnswer}" to the database.`);
+          
+        } 
+        else {
+            return "Please give a response";
+        }
+    
+    }
+    },
+                          //back to question 1 here
+    {
+     type: 'input',
+     message: 'What is the name of the role?',
+     name: 'name'
+    },
+    {
+    type: 'input',
+    message: 'What is the salary of the role?',
+    name: 'salary'
+    },
+    {
+    type: 'list',
+    message: 'Which department does the role belong to?',
+    choices: ['Engineering', "Finance", "Legal", "Sales", "Service"],
+    name: 'deptrole',
+    validate: function (userAnswer) {
+      if (userAnswer.length>0) {
+          console.log (`Added ${userAnswer} to the database.`);
+         
+      } 
+      else {
+          return "Please give a response";
       }
+      return true;
+      //message 'added ${userAnswer}...' go back to question 1
+  }
+  },
+  {
+    type: 'input',
+    message: `What is the employee's first name?`,
+    name: 'firstname'
+  },
+  {
+    type: 'input',
+    message: `What is the employee's last name?`,
+    name: 'lastname'
+  },
+  {
+  type: 'list',
+  message: `What is the employee's role?`,
+  choices: ['Sales Lead', "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
+  name: 'employeerole'
+  },
+  {
+  type: 'list',
+  message: `Who is the employee's manager?`,
+  choices: ['None', "John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown", "Sarah Lourd", "Tom Allen"],
+  name: 'manager'
+                                //back to first question
+  },
+  {
+  type: 'list',
+  message: `Which employee's role do you want to update?`,
+  choices: ['Malia Brown', "Sarah Lourd", "Tom Allen", "Sam Kash", "John Doe", "Mike Chan", "Ashley Rodriguez"],
+  name: 'roleupdate' 
+  },
+  {
+  type: 'list',
+  message: `Which role do you want to assign the selected employee?`,
+  choices: ['Sales Lead', "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
+  name: 'employeerole'
+          
+  },
+
 ];
 
 function changeThisFunctionName1(fileName, dataObject) {
@@ -17,13 +107,12 @@ function changeThisFunctionName1(fileName, dataObject) {
    
   }
 
-  function runQuestion() {
-    inquirer.prompt(question)
-  
+  function runQuestions() {
+    inquirer.prompt(questions)
   
       .then(answersObject=> {
      
-          changeThisFunctionName1('file.path', answersObject); 
+          changeThisFunctionName1('', answersObject); 
           console.log(answersObject);
           })
         .catch(error => {
@@ -32,5 +121,5 @@ function changeThisFunctionName1(fileName, dataObject) {
       };
   
   //calling runQuestions function
-  runQuestion();
+  runQuestions();
 
