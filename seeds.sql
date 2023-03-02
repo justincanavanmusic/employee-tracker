32451dd24a7ccc7c4e1d0e008173b6361365287a
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-01 19:36:32 -0600

    added role id and manager id w querys

--- seeds.sql
+++ seeds.sql
@@ -12,14 +12,14 @@ INSERT INTO role (title, salary, department_id)
   ("Legal Team Lead", 250000, 4), 
   ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-  ("John", "Doe", 1), 
-  ("Mike", "Chan", 2),
-  ("Ashley", "Rodriguez", 3), 
-  ("Kevin", "Tupik", 4),
-  ("Kunal", "Singh", 5), 
-  ("Malia", "Brown", 6), 
-  ("Sarah", "Lourd", 7), 
-  ("Tom", "Allen", 8);
+  ("John", "Doe", 1, null), 
+  ("Mike", "Chan", 2, 1),
+  ("Ashley", "Rodriguez", 3, null), 
+  ("Kevin", "Tupik", 4, 3),
+  ("Kunal", "Singh", 5, null), 
+  ("Malia", "Brown", 6, 5),
+  ("Sarah", "Lourd", 7, null), 
+  ("Tom", "Allen", 8, 7);
 
